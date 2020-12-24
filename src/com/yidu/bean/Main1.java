8a680e83ebc9a0b00b450c0dd3758686636254f2
18373457821tgh <2380499075@qq.com>
2020-12-24 16:31:09 +0800

    靓仔第二次项目

---
+++ src/com/yidu/bean/Main1.java
@@ -0,0 +1,13 @@
+package com.yidu.bean;
+
+/**
+ * @author 胖迪
+ * @version 1.0
+ * @description:
+ * @2020/12/24 16:30
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("靓女");
+    }
+}
